af92ec7b328be93c289e85b4536dba91631e889e
Félix Girault <felix.girault@gmail.com>
2013-01-03 21:06:27 +0100

    Fixes

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width" />  
 		<meta name="description" content="<?php echo fgDescription( ); ?>" />
 
 		<title><?php echo wp_title( ' — ', false, 'right' ) . get_bloginfo( 'name' ); ?></title>
