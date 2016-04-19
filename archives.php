b146c35c9ffa7e762bf4bcae536d1855f4e14673
Félix Girault <felix.girault@gmail.com>
2016-04-19 02:25:41 +0200

    Style adjustements

--- archives.php
+++ archives.php
@@ -13,42 +13,44 @@
 		</h1>
 	</header>
 
-	<section>
-		<header>
-			<h2>Articles</h2>
-		</header>
+	<div class="entry-body archives">
+		<section class="archives-section archives-section-articles">
+			<header>
+				<h2>Articles</h2>
+			</header>
 
-		<div class="content">
-			<ul>
-				<?php
-					wp_get_archives(
-						array(
-							'type' => 'postbypost'
-						)
-					);
-				?>
-			</ul>
-		</div>
-	</section>
+			<div class="content">
+				<ul>
+					<?php
+						wp_get_archives(
+							array(
+								'type' => 'postbypost'
+							)
+						);
+					?>
+				</ul>
+			</div>
+		</section>
 
-	<section>
-		<header>
-			<h2>Categories</h2>
-		</header>
+		<section class="archives-section archives-section-categories">
+			<header>
+				<h2>Categories</h2>
+			</header>
 
-		<div class="content">
-			<ul>
-				<?php
-					wp_list_categories(
-						array(
-							'title_li' => '',
-							'show_count' => true
-						)
-					);
-				?>
-			</ul>
-		</div>
-	</section>
+			<div class="content">
+				<ul>
+					<?php
+						wp_list_categories(
+							array(
+								'title_li' => '',
+								'show_count' => true
+							)
+						);
+					?>
+				</ul>
+			</div>
+		</section>
+	</div>
 </article>
 
 <?php get_footer(); ?>
