efd92f4195756c87e0f18da62feeb5bc9ca8c24b
Félix Girault <felix.girault@gmail.com>
2019-01-21 18:10:32 +0100

    Removed analytics
    Because fuck that.

--- header.php
+++ header.php
@@ -16,24 +16,6 @@
 	</head>
 
 	<body lang="fr" itemscope itemtype="http://schema.org/Blog">
-		<?php if (!is_user_logged_in()): ?>
-			<script type="text/javascript">
-				var _gaq = _gaq || [];
-				_gaq.push(['_setAccount', 'UA-11935716-3']);
-				_gaq.push(['_trackPageview']);
-
-				(function() {
-					var ga = document.createElement('script');
-					ga.type = 'text/javascript';
-					ga.async = true;
-					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-
-					var s = document.getElementsByTagName('script')[0];
-					s.parentNode.insertBefore(ga, s);
-				})();
-			</script>
-		<?php endif; ?>
-
 		<header class="page-header">
 			<p class="page-branding" role="banner" itemprop="name">
 				<span itemprop="author" itemscope itemtype="http://schema.org/Person">
